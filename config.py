971834c1b157310eae41021ce520334cba00bcf3
ArunKumar <arun.akumar-ak@protonmail.com>
2021-07-16 10:54:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ class Config:
     SECRET_KEY = os.urandom(64)
     USERNAME = os.getenv('USERNAME')
     PASSWORD = os.getenv('PASSWORD')
-    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://brtzdqrdjxljnt:154cecb747717cc481cdcfa5f063c63f05b34600d062b08208f2328afe483847@ec2-54-157-100-65.compute-1.amazonaws.com:5432/dar4i11itgigor"
+    SQLALCHEMY_DATABASE_URI = "sqlite:memory"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SCHEDULER_API_ENABLED = True
 
